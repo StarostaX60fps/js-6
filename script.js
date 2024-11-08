f58495d770ae09eedb56d8b0668cb818fdd9ddeb
StarostaX60fps <starosta.original@gmail.com>
2024-11-08 15:30:14 +0100

    HW-js-6

---
+++ script.js
@@ -0,0 +1,58 @@
+//    1
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// };
+
+//    2
+
+// for (let j = 2; j <= 20; j++) {
+//     if (j % 2 !== 0) continue;
+//     console.log(j);
+// };
+
+//    3
+
+// for (let k = 1; k <= 10; k++) {
+//     console.log(`7 * ${k} = ${7 * k}`);
+// };
+
+//    4
+
+// const arr1 = [1, 2, 3, 4, 5];
+// let index = 0;
+// while (index < arr1.length) {
+//     console.log(arr1[index]);
+//     index++;
+// };
+
+//    5
+
+// const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let m = 0; m < arr2.length; m++) {
+//     if (arr2[m] === 7) break;
+//     console.log(arr2[m]);
+// };
+
+//    6
+
+// const n = 10;
+// for (let p = 1; ; p++) {
+//     if (p >= n) break;
+//     console.log(p);
+// };
+
+//    7
+
+// let q = 1;
+// while (q <= 20) {
+//     if (q % 3 === 0) {
+//         q++;
+//         continue;
+//     }
+//     console.log(q);
+//     q++;
+// };
+
